1a9028cad532eae7df7b61ab905d409d2353a480
kishan <saikishan92@gmail.com>
2017-10-06 16:46:36 -0500

    checkstyle errors fixed

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,9 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/** {@inheritDoc}
+ */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
